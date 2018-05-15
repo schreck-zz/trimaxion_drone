841944f2a80629cb96fb6b9946d0e7cd52f478d6
schreck <schreckengost@gmail.com>
2018-05-15 11:25:58 -0400

    added unity project files, and weird cube field experiment

---
+++ Assets/CubeField.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeField : MonoBehaviour {
+
+    public Transform prefabelement;
+    public float spacing;
+    public int w;
+
+	// Use this for initialization
+	void Start () {
+        Transform foo;
+        float mid = spacing * w / 2;
+		for(int i = 0; i < w; i++)
+        {
+            for(int j = 0; j < w; j++)
+            {
+                for (int k = 0; k < w; k++)
+                {
+                    foo = Instantiate(prefabelement, transform.parent);
+                    foo.position = new Vector3(spacing * i - mid, spacing * j - mid, spacing * k - mid);
+                    foo.rotation = Random.rotation;
+                    foo.localScale = Random.Range(.7f, spacing / 10)*Vector3.one;
+                }
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
