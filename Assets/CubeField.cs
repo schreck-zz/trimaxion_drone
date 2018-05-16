3c895a98fffd73547c5682155b2a08bc59fb1fc0
schreck <schreckengost@gmail.com>
2018-05-16 06:37:37 -0400

    working on cube field

--- Assets/CubeField.cs
+++ Assets/CubeField.cs
@@ -2,14 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+
 public class CubeField : MonoBehaviour {
 
     public Transform prefabelement;
     public float spacing;
     public int w;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         Transform foo;
         float mid = spacing * w / 2;
         for (int i = 0; i < w; i++)
@@ -18,7 +21,7 @@ public class CubeField : MonoBehaviour {
             {
                 for (int k = 0; k < w; k++)
                 {
-                    foo = Instantiate(prefabelement, transform.parent);
+                    foo = Instantiate(prefabelement, transform);
                     foo.position = new Vector3(Random.Range(-spacing/3,spacing/3)+spacing * i - mid, Random.Range(-spacing / 3, spacing / 3) + spacing * j - mid, Random.Range(-spacing / 3, spacing / 3) + spacing * k - mid);
                     foo.rotation = Random.rotation;
                     foo.localScale = Random.Range(.7f, spacing / 10)*Vector3.one;
